b5e9ded9821247c97738a580b5e02057fe8d562e
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-16 12:09:36 +0100

    Exo7

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $isOk=false;
+    if ($isOk == false) {
+      echo 'C\'est pas bon !!!!';
+    }
+    else {
+      echo 'C\'est ok !!!!';
+    }
+    ?>
+  </body>
